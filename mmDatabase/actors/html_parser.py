f4d36fc8feddf9d7ad420198fd38cd8002012172
engikeer <engikeer@163.com>
2017-03-26 19:06:18 +0800

    update

--- mmDatabase/actors/html_parser.py
+++ mmDatabase/actors/html_parser.py
@@ -60,6 +60,7 @@ class HtmlParser(object):
 
     def _get_new_urls(self, menu_url, soup):
         new_urls = set()
+        
         # /girl/23790/
         links = soup.find_all("a", href=re.compile(r"/girl/\d+/"))
         for link in links:
