b7bbf45e2b63c061821ef54ccccf828e14987ad3
engikeer <engikeer@163.com>
2017-04-07 19:52:03 +0800

    update

---
+++ sample/orm_sample.py
@@ -0,0 +1,20 @@
+# 负责保存数据库表的字段名和字段类型
+class Field(object):
+    def __init__(self, name, column_type):
+        self.name = name
+        self.column_type = column_type
+
+    def __str__(self):
+        return '<%s:%s>' % (self.__class__.__name__, self.name)
+
+
+# 保存不通类型的字段名和字段类型
+class StringField(Field):
+    def __init__(self, name):
+        super(StringField, self).__init__(name, 'varchar(100)')
+
+
+class IntegerField(Field):
+    def __init__(self, name):
+        super(IntegerField, self).__init__(name, 'bigint')
+
